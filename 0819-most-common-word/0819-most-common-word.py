1f170ec3b71569763100c79584cbf3b98d6ef91b
Harshith <kalvaharshith@gmail.com>
2026-06-02 11:16:33 +0530

    Time: 0 ms (100%), Space: 19.2 MB (84.95%) - LeetHub

---
+++ 0819-most-common-word/0819-most-common-word.py
@@ -0,0 +1,11 @@
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        for ch in "!?',;.":
+            paragraph = paragraph.replace(ch, " ")
+        words = paragraph.lower().split()
+        banned = set(banned)
+        freq = {}
+        for word in words:
+            if word not in banned:
+                freq[word] = freq.get(word, 0) + 1
+        return max(freq, key=freq.get)
