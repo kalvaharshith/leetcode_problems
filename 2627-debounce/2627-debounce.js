99fbd69679525e043964c1f76e44b26abbb2b282
Harshith <kalvaharshith@gmail.com>
2026-06-06 22:30:49 +0530

    Time: 48 ms (66.6%), Space: 53.9 MB (61.56%) - LeetHub

---
+++ 2627-debounce/2627-debounce.js
@@ -0,0 +1,16 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var debounce = function(fn, t) {
+    let timeoutId;
+
+    return function(...args) {
+        clearTimeout(timeoutId);
+
+        timeoutId = setTimeout(() => {
+            fn(...args);
+        }, t);
+    };
+};
